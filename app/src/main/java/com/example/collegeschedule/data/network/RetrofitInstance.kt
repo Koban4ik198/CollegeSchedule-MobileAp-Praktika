f676b297d7c67f859268e8fb6f97192ee15ebd44
K0ban4ik7089 <borissviridov24@gmail.com>
2026-01-15 18:08:55 +0700

    Созлдание API-интерфейса Retrofit, Retrofit-клиента, репозитория, утилиты для работы с датами

---
+++ app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.example.collegeschedule.data.network
+
+import com.example.collegeschedule.data.api.ScheduleApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "http://10.0.2.2:5141/"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api: ScheduleApi = retrofit.create(ScheduleApi::class.java)
+}
